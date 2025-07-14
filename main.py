722e6c84c91f26a10894d986a7133309c04ee967
Vedant Rajurkar <rajurkarvedant8@gmail.com>
2025-07-14 22:56:00 +0530

    dataset_update

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import gradio as gr
 from transformers import pipeline
 
-# Load a proper instruction-tuned chat model
-chatbot = pipeline("text2text-generation", model="google/flan-t5-small")
+chatbot = pipeline("text2text-generation", model="gpt2")
 
 def chat_fn(message):
     prompt = f"Answer the following question clearly:\n{message}"
-    result = chatbot(prompt, max_new_tokens=100)[0]["generated_text"]
-    return result
+    result = chatbot(prompt,   max_new_tokens=100,
+        temperature=1,             # add randomness
+        top_k=100,                    # limit to top 50 predictions
+        top_p=0.95,                  # nucleus sampling
+        repetition_penalty=1,      # reduce repeating loops
+        do_sample=True               # allow sampling
+    )[0]["generated_text"]
+    response = result[len(prompt):].strip()
+    return response
 
 gr.Interface(fn=chat_fn, inputs="text", outputs="text", title="My Hugging Face AI Agent").launch()
